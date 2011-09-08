51cf9b12304ce63c535c28c3e202c2538b9f8a4e
Robert Simmons <xzao99x@gmail.com>
2011-09-08 18:32:12 -0500

    Whooops. Forgot to remove unneeded project source.

--- Utilities/General/ISendEmails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Utilities.General
-{
-	public interface ISendEmails
-	{
-		void SendMail(string host, string to, string from, string cc, string subject, string content);
-
-		void SendMail(string host, string to, string from, string cc, string subject, string content, string[] attachments);
-	}
-}
--- Utilities/Mapping/IMap.cs
+++
@@ -1,9 +0,0 @@
-using AutoMapper;
-
-namespace Utilities.Mapping
-{
-	public interface IMap
-	{
-		void CreateMappings(IConfiguration mappingConfig);
-	}
-}
